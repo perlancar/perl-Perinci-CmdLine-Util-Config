da21e5230e80609300ba89959250fd8c9bd13266
perlancar (@pc-office) <perlancar@gmail.com>
2017-01-04 15:21:59 +0700

    Replace Log::Any::IfLOG with Log::Any to make this dist easier to package for Debian

--- lib/Perinci/CmdLine/Util/Config.pm
+++ lib/Perinci/CmdLine/Util/Config.pm
@@ -6,7 +6,7 @@ package Perinci::CmdLine::Util::Config;
 use 5.010001;
 use strict;
 use warnings;
-use Log::Any::IfLOG '$log';
+use Log::Any '$log';
 
 use PERLANCAR::File::HomeDir qw(get_my_home_dir);
 
